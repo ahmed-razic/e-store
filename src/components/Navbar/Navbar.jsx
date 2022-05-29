a2169e2218e1cfc262cc9c7a91d6d885b30d1fb0
ahmed-razic <ahmed.razic@gmail.com>
2022-05-29 12:02:55 +0200

    Navbar comp

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Badge,
+  MenuItem,
+  Menu,
+  Typography,
+} from '@material-ui/core'
+import { AddShoppingCart } from '@material-ui/icons'
+
+const Navbar = () => {
+  return (
+    <>
+      <AppBar></AppBar>
+    </>
+  )
+}
+export default Navbar
